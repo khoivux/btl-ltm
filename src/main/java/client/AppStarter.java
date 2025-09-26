db7d1e7228c7085ded4826bc83aed6a078de8fe1
khoivux <khoivux21@gmail.com>
2025-09-26 21:54:35 +0700

    feat(): base client-server and login

---
+++ src/main/java/client/AppStarter.java
@@ -0,0 +1,44 @@
+package client;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import model.Message;
+
+import java.io.IOException;
+
+public class AppStarter extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            Client client = new Client(stage);
+            client.showLoginUI();
+            new Thread(() -> {
+                try {
+                    client.startConnection("127.0.0.1", 23456);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // UI phải cập nhật trên JavaFX Thread
+                    Platform.runLater(() -> client.showErrorAlert("Không thể kết nối tới server."));
+                }
+            }).start();
+
+            // Xử lý khi người dùng đóng cửa sổ
+            stage.setOnCloseRequest(event -> {
+                try {
+                    if (client.getUser() != null) {
+                        Message logoutMessage = new Message("logout", client.getUser().getId());
+                        client.sendMessage(logoutMessage);
+                    }
+                    client.closeConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Platform.exit();
+        }
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int id;
+    private String username;
+    private String password;
+    private int points;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(int id, String username, int points) {
+        this.id = id;
+        this.username = username;
+        this.points = points;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
